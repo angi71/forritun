80712b812e6b5f67d858dfc7381eb9cd91ff3a20
angi71 <angi80086@gmail.com>
2018-10-04 16:23:37 +0000

    jhhg

---
+++ minir_klasar.py
@@ -0,0 +1,10 @@
+#Árbjartur angi
+#4.10.2018
+
+class Lagerhlutur:
+    def __init__(self, numer, tegund,fjoldi,verd):
+        self.numer = numer
+        self.tegund = tegund
+        self.fjoldi = fjoldi
+        self.verd = verd
+
